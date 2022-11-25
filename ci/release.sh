c058bab47eda247eed178e6a7159a317fe6a5a9a
idirall22 <idirall22@gmail.com>
2022-11-25 17:09:09 +0100

    export report 10-2022

--- ci/release.sh
+++ ci/release.sh
@@ -7,16 +7,7 @@ source ci/ci.env
 npm install
 
 # export report Polygon
-# node index.js year=$YEAR month=$MONTH
-node index.js year=$YEAR month=9 useFile=true
-node index.js year=$YEAR month=8 useFile=true
-node index.js year=$YEAR month=7 useFile=true
-node index.js year=$YEAR month=6 useFile=true
-node index.js year=$YEAR month=5 useFile=true
-node index.js year=$YEAR month=4 useFile=true
-node index.js year=$YEAR month=3 useFile=true
-node index.js year=$YEAR month=2 useFile=true
-node index.js year=$YEAR month=1 useFile=true
+node index.js year=$YEAR month=$MONTH
 
 # export report Near
-# node near.js year=$YEAR month=$MONTH file=$FILE
+node near.js year=$YEAR month=$MONTH file=$FILE
